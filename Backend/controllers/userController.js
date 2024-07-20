1b0137024340896e5065f1eecc76d41ba5b25238
Leela-Avinash <leelaavinash24@gmail.com>
2024-07-20 15:29:51 +0530

    add login, logout, follow and unfollow functionalities

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/helpers/generateToken.js";
 
 const signupUser = async (req, res) => {
     try {
@@ -20,6 +21,8 @@ const signupUser = async (req, res) => {
         });
 
         if(user){
+            generateTokenAndSetCookie(user._id, res);
+
             res.status(201).json({
                 _id: user._id,
                 name: user.name,
@@ -35,4 +38,69 @@ const signupUser = async (req, res) => {
     }
 }
 
-export {signupUser};
+const loginUser = async (req, res)=>{
+    try {
+        const {username, password} = req.body;
+        let user = await User.findOne({ username });
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+        
+        if(!user || !isPasswordCorrect) {
+            return res.status(400).json({message : "Invalid Username or password"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            username: user.username,
+            email: user.email
+        });
+
+    } catch (err) {
+        res.status(500).json({message: err.message});
+        console.log("Error in login user: ", err.message);       
+    }
+}
+
+const logoutUser = (req, res) => {
+    try {
+        res.cookie("jwt", "", {maxAge: 1});
+        res.status(200).json({message: "Logged out Successfully"});   
+    } catch (err) {
+        res.status(500).json({message: err.message});
+        console.log("Error in logout user: ", err.message);
+    }
+};
+
+const followUnfollow = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const targetUser = await User.findById(id);
+        const currentuser = await User.findById(req.user._id);
+
+        if(id === req.user._id){
+            req.status(400).json({message: "you cannot follow / unfollow yourself"});
+        }
+
+        if(!targetUser || !currentuser) {
+            res.status(400).json({message: "user not found"});
+        }
+
+        const isFollowing = currentuser.following.includes(id);
+
+        if(isFollowing){
+            await User.findByIdAndUpdate(currentuser._id, { $pull: {following: id}});
+            await User.findByIdAndUpdate(id, { $pull : {followers: currentuser._id}});
+        } else {
+            await User.findByIdAndUpdate(currentuser._id, { $push: {following: id}});
+            await User.findByIdAndUpdate(id, { $push : {followers: currentuser._id}});
+        }
+        res.status(200).json({message: "followed successfully"});
+    } catch(err) {
+        res.status(500).json({message: err.message});
+        console.log("Error in followUnfollow user: ", err.message);
+    }
+}
+
+export {signupUser, loginUser, logoutUser, followUnfollow };
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { signupUser } from "../controllers/userController.js";
+import { followUnfollow, loginUser, logoutUser, signupUser } from "../controllers/userController.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post('/signup', signupUser)
-
+router.post('/signup', signupUser);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
+router.post('/follow/:id', protectRoute, followUnfollow)
 export default router;
